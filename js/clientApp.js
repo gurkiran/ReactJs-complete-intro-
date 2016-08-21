d6b93258f02994af9d1e7cf9909a13eee8aae840
gsinghla <gsinghla@mac-dcb05-325.home>
2016-08-20 22:53:53 -0400

    adding props

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,9 +1,24 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+  render (){
+    return (
+        div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
+
 var MyFirstComponent = (
   div(null,
-    h1(null, 'This is my first component')
+    MyTitleFact({title: 'This is the first paragraph.', color: 'rebeccapurple'}),
+    ce(MyTitle, {title: 'This is the second paragraph.', color: 'mediumaquamarine'}),
+    React.createElement(MyTitle, {title: 'This is the third paragraph.', color: 'peru'})
   )
 )
 
